6f2b696ffdc63b1b51020eb841ddbd4be421fa5b
David Bears <dbear1@vt.edu>
2021-06-08 18:04:48 -0400

    minimal scalatests for lz77

---
+++ src/test/scala/LZ77DecompressorTest.scala
@@ -0,0 +1,55 @@
+package lz77
+
+import lz77Decompressor.lz77Decompressor
+import lz77Parameters.lz77Parameters
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+import org.scalatest._
+import org.scalatest.flatspec._
+import matchers.should._
+import chisel3.tester._
+import chisel3.experimental.BundleLiterals._
+import chisel3.stage.ChiselStage
+import java.io._
+
+class LZ77DecompressorTestUncompressable(
+    lz77: lz77Decompressor,
+    params: lz77Parameters)
+    extends PeekPokeTester[lz77Decompressor](lz77) {
+  
+  val data = Seq(0, 1, 2, 3, 4, 5, 6, 7)
+  var inidx = 0
+  var outidx = 0
+  poke(lz77.io.in.finished, false)
+  while(peek(lz77.io.out.finished) == 0) {
+    if(inidx < data.length) {
+      poke(lz77.io.in.bits(0), data(inidx))
+      poke(lz77.io.in.valid, 1)
+      poke(lz77.io.out.ready, 1)
+    }
+    else {
+      poke(lz77.io.in.finished, true)
+    }
+    if(peek(lz77.io.out.valid) > 0) {
+      println(peek(lz77.io.out.bits(0)).toString)
+      expect(lz77.io.out.bits(0), data(outidx))
+      outidx = outidx + 1
+    }
+    if(peek(lz77.io.in.ready) > 0) {
+      inidx = inidx + 1
+    }
+    step(1)
+  }
+  if(outidx != data.length)
+    fail
+}
+
+
+class LZ77DecompressorTest extends AnyFlatSpec with Matchers {
+  "LZ77DecompressorTestUncompressable" should "pass" in {
+    val params = new getLZ77FromCSV().getLZ77FromCSV("configFiles/lz77.csv")
+    chisel3.iotesters.Driver(() => new lz77Decompressor(params))
+      {lz77 => new LZ77DecompressorTestUncompressable(lz77, params)}
+  }
+}
---
+++ src/test/scala/LZ77Test.scala
@@ -0,0 +1,45 @@
+package lz77
+
+import lz77CompressorDecompressor.lz77CompressorDecompressor
+import lz77Parameters.lz77Parameters
+import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+import org.scalatest._
+import org.scalatest.flatspec._
+import matchers.should._
+import chisel3.tester._
+import chisel3.experimental.BundleLiterals._
+import chisel3.stage.ChiselStage
+import java.io._
+
+class LZ77Test(
+    lz77: lz77CompressorDecompressor,
+    params: lz77Parameters,
+    datastream: InputStream)
+    extends PeekPokeTester[lz77CompressorDecompressor](lz77) {
+  
+  val dataarray = new Array[Byte](
+    (params.charactersToCompress * params.characterBits + 7) / 8)
+  val remainder =
+    dataarray.length * 8 - params.charactersToCompress * params.characterBits
+  while(datastream.read(dataarray) != -1) {
+    val dataint = BigInt(dataarray)
+    poke(lz77.io.in.asUInt, dataint)
+    while(peek(lz77.io.finished) == 0)
+      step(1)
+    expect(lz77.io.out.asUInt, dataint)
+    println("pass")
+    reset()
+  }
+}
+
+
+class LZ77TestImagick extends AnyFlatSpec with Matchers {
+  "LZ77TestImagick" should "pass" in {
+    val params = new getLZ77FromCSV().getLZ77FromCSV("configFiles/lz77.csv")
+    val datastream = new FileInputStream("dumps/parseddumpfiles/imagickparsed")
+    chisel3.iotesters.Driver(() => new lz77CompressorDecompressor(params))
+      {lz77 => new LZ77Test(lz77, params, datastream)}
+  }
+}
