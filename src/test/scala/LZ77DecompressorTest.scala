698a8acc3e4f9a9558ffa0d3ec57412735cfb08d
David Bears <dbear1@vt.edu>
2021-06-25 14:26:41 -0400

    modified decompressor test to be extensible to compressor

--- src/test/scala/LZ77DecompressorTest.scala
+++ src/test/scala/LZ77DecompressorTest.scala
@@ -11,66 +11,7 @@ import matchers.should._
 import chisel3.tester._
 import chisel3.experimental.BundleLiterals._
 import chisel3.stage.ChiselStage
-import java.io._
 import scala.util.control.Breaks._
-
-class LZ77DecompressorTester(
-    lz77: lz77Decompressor,
-    params: lz77Parameters,
-    input: Seq[Int],
-    expected: Seq[Int])
-    extends PeekPokeTester[lz77Decompressor](lz77) {
-  
-  // initialize inputs
-  for(index <- 0 until lz77.io.in.bits.length)
-    poke(lz77.io.in.bits(index), 0)
-  poke(lz77.io.in.valid, 0)
-  poke(lz77.io.out.ready, 0)
-  poke(lz77.io.in.finished, false)
-  
-  var inidx = 0
-  var outidx = 0
-  
-  var timeout = 10000
-  while(peek(lz77.io.out.finished) == 0 && timeout > 0) {
-    poke(lz77.io.in.valid, 0)
-    poke(lz77.io.out.ready, 0)
-    poke(lz77.io.in.finished, true)
-    for(i <- 0 until
-        (lz77.io.in.bits.length min (input.length - inidx))) {
-      poke(lz77.io.in.bits(i), input(inidx + i))
-      poke(lz77.io.in.valid, i + 1)
-      poke(lz77.io.in.finished, false)
-    }
-    
-    poke(lz77.io.out.ready, lz77.io.out.bits.length)
-    
-    // println(s"out valid = ${peek(lz77.io.out.valid)}; in ready = ${peek(lz77.io.in.ready)}")
-    
-    inidx += (peek(lz77.io.in.ready) min (peek(lz77.io.in.valid))).intValue
-    
-    for(i <- 0 until
-        (peek(lz77.io.out.ready) min (peek(lz77.io.out.valid))).intValue) {
-      if(outidx + i < expected.length)
-        expect(lz77.io.out.bits(i), expected(outidx + i))
-      else {
-        println(s"${i}: ${peek(lz77.io.out.bits(i))} (indexed past end of data: ${outidx + i})")
-        fail
-      }
-    }
-    
-    outidx += (peek(lz77.io.out.ready) min (peek(lz77.io.out.valid))).intValue
-    
-    step(1)
-    timeout -= 1
-  }
-  
-  if(outidx != expected.length) {
-    println(s"outidx was ${outidx}; Expected ${expected.length}")
-    fail
-  }
-}
-
 
 class LZ77DecompressorTest extends AnyFlatSpec with Matchers {
   
@@ -156,7 +97,7 @@ class LZ77DecompressorTest extends AnyFlatSpec with Matchers {
       expect.flatMap(d => if(d == params.escapeCharacter) Seq(d, d) else Seq(d))
     
     chisel3.iotesters.Driver(() => new lz77Decompressor(params)){lz77 =>
-      new LZ77DecompressorTester(lz77, params, input, expect)
+      new StreamComparisonTester(lz77, input, expect)
     } should be (true)
   }
   
@@ -167,7 +108,7 @@ class LZ77DecompressorTest extends AnyFlatSpec with Matchers {
     chisel3.iotesters.Driver.execute(Array(),
       () => new lz77Decompressor(params))
     {lz77 =>
-      new LZ77DecompressorTester(lz77, params, input, expect)
+      new StreamComparisonTester(lz77, input, expect)
     } should be (true)
   }
   
@@ -178,7 +119,7 @@ class LZ77DecompressorTest extends AnyFlatSpec with Matchers {
     chisel3.iotesters.Driver.execute(Array(),
       () => new lz77Decompressor(params))
     {lz77 =>
-      new LZ77DecompressorTester(lz77, params, input, expect)
+      new StreamComparisonTester(lz77, input, expect)
     } should be (true)
   }
 }
