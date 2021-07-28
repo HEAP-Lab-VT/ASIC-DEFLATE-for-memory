0f6881c00fecc89f5b847341e1f5d04ca574f6cb
David Bears <dbear1@vt.edu>
2021-07-28 14:56:19 -0400

    remove cleaning from verilator script

--- scripts/verilatorLZ77.sh
+++ scripts/verilatorLZ77.sh
@@ -1,4 +1,2 @@
-rm ./obj_dir/V*
-rm ./obj_dir/*.[adoh]
 verilator -Wno-WIDTH --cc LZ77Compressor.v --exe LZ77Compressor.cpp $1 && make -j12 -C obj_dir/ -f VLZ77Compressor.mk VLZ77Compressor
 verilator -Wno-WIDTH --cc LZ77Decompressor.v --exe LZ77Decompressor.cpp $1 && make -j12 -C obj_dir/ -f VLZ77Decompressor.mk VLZ77Decompressor
