dfe920f6886a329d6e0b922686147bb602e555fc
David Bears <dbear1@vt.edu>
2021-09-25 17:09:30 -0400

    add cleaning to verilator script

--- scripts/verilatorLZ77.sh
+++ scripts/verilatorLZ77.sh
@@ -1,2 +1,3 @@
+rm obj_dir/V*
 verilator -Wno-WIDTH --cc LZ77Compressor.v --exe LZ77Compressor.cpp $1 && make -j12 -C obj_dir/ -f VLZ77Compressor.mk VLZ77Compressor
 verilator -Wno-WIDTH --cc LZ77Decompressor.v --exe LZ77Decompressor.cpp $1 && make -j12 -C obj_dir/ -f VLZ77Decompressor.mk VLZ77Decompressor
