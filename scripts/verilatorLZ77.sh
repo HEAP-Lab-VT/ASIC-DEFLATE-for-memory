2e44645794d26d880f25b2d695d32599512c8d32
David Bears <dbear1@vt.edu>
2021-10-30 21:27:52 -0400

    remove job limit on make

--- scripts/verilatorLZ77.sh
+++ scripts/verilatorLZ77.sh
@@ -29,5 +29,5 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
 rm obj_dir/V*
-verilator -Wno-WIDTH --cc LZ77Compressor.v --exe LZ77Compressor.cpp $traceargs && make -j12 -C obj_dir/ -f VLZ77Compressor.mk VLZ77Compressor
-verilator -Wno-WIDTH --cc LZ77Decompressor.v --exe LZ77Decompressor.cpp $traceargs && make -j12 -C obj_dir/ -f VLZ77Decompressor.mk VLZ77Decompressor
+verilator -Wno-WIDTH --cc LZ77Compressor.v --exe LZ77Compressor.cpp $traceargs && make -j -C obj_dir/ -f VLZ77Compressor.mk VLZ77Compressor
+verilator -Wno-WIDTH --cc LZ77Decompressor.v --exe LZ77Decompressor.cpp $traceargs && make -j -C obj_dir/ -f VLZ77Decompressor.mk VLZ77Decompressor
