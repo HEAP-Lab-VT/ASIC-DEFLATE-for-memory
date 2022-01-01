6f5dce28892b616f9552f75faec1b3431267a454
David Bears <dbear1@vt.edu>
2022-01-01 18:10:46 -0500

    verilator scripts upgrade

--- scripts/verilatorLZ77.sh
+++ scripts/verilatorLZ77.sh
@@ -29,5 +29,10 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
 rm obj_dir/V*
-verilator -Wno-WIDTH --cc LZ77Compressor.v --exe LZ77Compressor.cpp $traceargs && make -j -C obj_dir/ -f VLZ77Compressor.mk VLZ77Compressor
-verilator -Wno-WIDTH --cc LZ77Decompressor.v --exe LZ77Decompressor.cpp $traceargs && make -j -C obj_dir/ -f VLZ77Decompressor.mk VLZ77Decompressor
+rm obj_dir/*.{o,d}
+mod=LZ77Compressor
+verilator -Wno-WIDTH --cc $mod.v --exe DecoupledStreamModule.cpp -CFLAGS "-D 'MODNAME=$mod' -D 'IN_CHARS=11' -D 'OUT_CHARS=8'" $traceargs && make -j -C obj_dir/ -f V$mod.mk V$mod
+
+rm obj_dir/DecoupledStreamModule.{o,d}
+mod=LZ77Decompressor
+verilator -Wno-WIDTH --cc $mod.v --exe DecoupledStreamModule.cpp -CFLAGS "-D 'MODNAME=$mod' -D 'IN_CHARS=8' -D 'OUT_CHARS=8'" $traceargs && make -j -C obj_dir/ -f V$mod.mk V$mod
