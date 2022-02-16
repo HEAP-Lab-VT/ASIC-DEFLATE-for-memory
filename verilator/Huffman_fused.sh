d235b919eaf4bbd9cc9d798abf45d1e59009313e
David Bears <dbear1@vt.edu>
2022-02-16 13:40:08 -0500

    create verilator framework for huffman testing

---
+++ verilator/Huffman_fused.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# argument parsing from https://stackoverflow.com/a/14203146/8353152
+traceargs=""
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  
+  case $key in
+    --trace)
+      traceargs+=" --trace -CFLAGS \"-DTRACE_ENABLE=true\""
+      shift # past argument
+      ;;
+    --no-trace)
+      traceargs+=" -CFLAGS \"-DTRACE_ENABLE=false\""
+      shift # past argument
+      ;;
+    --trace-underscore)
+      traceargs+=" --trace-underscore"
+      shift # past argument
+      ;;
+    *)    # unknown option
+      POSITIONAL+=("$1") # save it in an array for later
+      shift # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+er() {
+  echo "$@"
+  $@
+}
+
+amod=HuffmanCompressor
+verilator -Wno-WIDTH --cc $amod.v $traceargs &&
+make -j -C obj_dir/ -f V$amod.mk V${amod}__ALL.a
+
+bmod=HuffmanDecompressor
+verilator -Wno-WIDTH --cc $bmod.v --exe V${amod}__ALL.a ../verilator/Huffman_fused.cpp -CFLAGS '-ggdb' $traceargs &&
+make -j -C obj_dir/ -f V$bmod.mk V${bmod} &&
+cp obj_dir/VHuffmanDecompressor obj_dir/VHuffman_fused
+
+# make -C obj_dir -f ../verilator/Huffman_fused.mk
+
+#er g++ -c -I. -I${VERILATOR_ROOT}/include ../verilator/Huffman_fused.cpp -o VHuffman_fused.o
+#er g++ Huffman_fused.o V${amod}__ALL.a V${bmod}__ALL.a -o VHuffman_fused
