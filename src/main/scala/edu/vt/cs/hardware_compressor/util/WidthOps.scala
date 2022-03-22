44b3ee6237bcd39a9f434c0bd57ff49c469fe288
David Bears <dbear1@vt.edu>
2022-03-21 20:14:08 -0400

    add/modify some implicit conversions

--- src/main/scala/edu/vt/cs/hardware_compressor/util/WidthOps.scala
+++ src/main/scala/edu/vt/cs/hardware_compressor/util/WidthOps.scala
@@ -3,20 +3,22 @@ package edu.vt.cs.hardware_compressor.util
 import chisel3._
 import chisel3.util._
 
+import scala.language.implicitConversions
+
 object WidthOps {
-  implicit class WidthOpsBigInt(v: BigInt) {
+  implicit class WidthOps_BigInt(v: BigInt) {
     def valBits(): Int = log2Ceil(v + 1)
     def idxBits(): Int = log2Ceil(v)
     def valUInt(): UInt = UInt(v.valBits.W)
     def idxUInt(): UInt = UInt(v.idxBits.W)
     def maxVal(): BigInt = (BigInt(1) << v.intValue) - 1
     def space(): BigInt = BigInt(1) << v.intValue
-    def isPow2(): Boolean = v == v.ceilPow2
+    def isPow2(): Boolean = v.bitCount == 1
     def ceilPow2(): BigInt = v.idxBits.space
   }
-  implicit class WidthOpsInt(v: Int) extends WidthOpsBigInt(v)
-  implicit class WidthOpsLong(v: Long) extends WidthOpsBigInt(v)
-  implicit class WidthOpsUInt(v: UInt) {
+  implicit class WidthOps_Int(v: Int) extends WidthOps_BigInt(v)
+  implicit class WidthOps_Long(v: Long) extends WidthOps_BigInt(v)
+  implicit class WidthOps_UInt(v: UInt) {
     def maxVal(): BigInt = (1 << v.getWidth) - 1
     def space(): BigInt = 1 << v.getWidth
   }
