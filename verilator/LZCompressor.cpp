b87d97ade976cdff2b519d469db160d5913c91d3
David Bears <dbear1@vt.edu>
2022-03-28 21:27:54 -0400

    create convenience files LZCompressor.ccp and LZDecompressor.ccp

---
+++ verilator/LZCompressor.cpp
@@ -0,0 +1,6 @@
+
+#define MODNAME LZCompressor
+#define IN_CHARS 11
+#define OUT_CHARS 8
+
+#include "DecoupledStreamModule.cpp"
---
+++ verilator/LZDecompressor.cpp
@@ -0,0 +1,6 @@
+
+#define MODNAME LZDecompressor
+#define IN_CHARS 8
+#define OUT_CHARS 8
+
+#include "DecoupledStreamModule.cpp"
